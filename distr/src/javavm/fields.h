f5a890f0becb3adc040cd8128cda0f851ca643c7
mzattera <b2fjava@gmail.com>
2017-06-28 21:09:25 +0200

    Update fields.h

--- distr/src/javavm/fields.h
+++ distr/src/javavm/fields.h
@@ -6,6 +6,4 @@
 #define get_pgfield_type(HIBYTE_)            ((HIBYTE_) >> 4)
 #define get_pgfield_offset(HIBYTE_,LOBYTE_)  (((TWOBYTES) ((HIBYTE_) & 0x0F) << 8) | (LOBYTE_))
 
-
-
 #endif // _FIELDS_H
