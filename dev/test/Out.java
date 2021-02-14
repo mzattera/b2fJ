6cb60a0f05674d1b4854b3292da92382aa5ce7f4
Eduardo Ahumada <eahumada@gmail.com>
2021-02-14 10:29:20 -0300

    Fix test classes to compile with IDE

--- dev/test/Out.java
+++ dev/test/Out.java
@@ -2,6 +2,20 @@ import b2fj.io.*;
 import b2fj.util.*;
 
 public class Out {
+	public static class AClass {
+		protected String name;
+		private AClass(String name) {
+			this.name=name;
+		}
+	}
+	public static class BClass extends AClass {
+		private BClass(String name, String lastname, double d) {
+			super(name);
+		}
+		void print() {
+			System.out.println(name);
+		}
+	}
 	public static void main(String[] args) {
 		// ConsoleOutputStream.getInstance().write(65);
 		// ConsolePrintStream.getInstance().println("Ciao.");
